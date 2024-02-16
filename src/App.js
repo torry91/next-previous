38b7c2f51376ba63961a49eab763dc4efe85f346
Victoria Badrajan <badrajanvictoria91@gmail.com>
2024-02-16 13:52:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { data } from "./data";
+import './App.css';
+
+function App() {
+  const [person, setPerson] = useState(0);
+  const {id, name, description, age, image} = data[person];
+
+  const nextPerson = () => {
+  setPerson ((person => {
+    person ++;
+    if (person > data.length-1){
+      person = 0;
+    }
+    return person;
+  }))
+}
+
+  const previousPerson = () => {
+    setPerson((person => {
+      person -- ;
+      if (person < 0) {
+        return data.length -1;
+      }
+      return person;
+    }))
+  }
+
+  return(<div>
+      <div className="container">
+        <img src={image} width="300px" height="375px" alt="pic"/>
+      </div>
+
+      <div className="container">
+      <h1>{id} - {name}</h1>
+      </div>
+
+    <div className="container">
+      <h2>{description}</h2>
+    </div>
+
+    <div className="container">
+      <h2>{age} years old</h2>
+    </div>
+
+    <div className="btn container">
+      <button onClick ={previousPerson} >Previous</button>
+      <button onClick = {nextPerson}>Next</button>
+    </div>
+
+    </div>)
+}
+export default App;
+
---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+        id: 1,
+        name:"Christine Campbell",
+        description: "Me + You +… JK, You Don’t Have to Do Any Math to Send Me a Message!",
+        age: 30 ,
+        image: "https://images.unsplash.com/photo-1660078445215-de85e9fc73f2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM4fHx3b21hbiUyMGdpcmwlMjBwaG90b3Nob290fGVufDB8fDB8fHww"
+    },
+    
+    { 
+        id: 2,
+        name: "Janet Smith",
+        description: "Tired of Online Dating? Let’s Be Each Other’s Reason for Signing Off",
+        age: 24,
+        image: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGh1bWFufGVufDB8fDB8fHww"
+    },
+    
+    {
+        id: 3,
+        name: "Andrew Brown",
+        description: "Book Worm Looking to Write the Next Chapter",
+        age: 32,
+        image: "https://media.istockphoto.com/photos/portrait-of-handsome-smiling-young-man-with-crossed-arms-picture-id1200677760?k=6&m=1200677760&s=612x612&w=0&h=IXURU5rnCRyN5h5ebeDLQcVv0lNs0ZLgE98fPioSMnQ="
+    },
+    
+    {
+        id: 4,
+        name: "Olivia Lee",
+        description: "Life is Great, But Maybe It Can Be Better With You?",
+        age: 26,
+        image: "https://images.unsplash.com/photo-1604072366595-e75dc92d6bdc?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fGh1bWFufGVufDB8fDB8fHww"
+    },
+    
+    {
+        id: 5,
+        name: "Lance Reis",
+        description: "Music Lover Looking to Write the Next Verse",
+        age: 31,
+        image: "https://images.unsplash.com/photo-1545959308-3f4b859ad821?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjE0fHxtYW58ZW58MHx8MHx8fDA%3D"
+    }
+    ]
